60e44cd3d119aba776bd00275049446886fd15ad
Anthony Nahas <anthony.na@hotmail.de>
2018-06-18 21:17:26 +0200

    feat(package): added MatValidateAddressDirective | `matValidateAddress`

---
+++ src/module/directives/address-validator/mat-address-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MatValidateAddressDirective } from './mat-address-validator.directive';
+
+describe('AddressValidator', () => {
+  it('should create an instance', () => {
+    const directive = new MatValidateAddressDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/module/directives/address-validator/mat-address-validator.directive.ts
@@ -0,0 +1,51 @@
+import {Directive, EventEmitter, forwardRef} from '@angular/core';
+import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn} from '@angular/forms';
+import {} from '@types/googlemaps';
+import PlaceResult = google.maps.places.PlaceResult;
+
+// https://github.com/angular/angular/blob/master/packages/forms/src/directives/validators.ts
+
+@Directive({
+  selector: '[mat-address-validate][formControlName],[MatValidateAddress][formControl],[MatValidateAddress][ngModel]',
+  providers: [
+    {provide: NG_VALIDATORS, useExisting: forwardRef(() => MatValidateAddressDirective), multi: true}
+  ]
+})
+export class MatValidateAddressDirective implements Validator {
+
+  public subscription: any;
+
+  private _address: PlaceResult;
+
+
+  constructor() {
+  }
+
+  public validate(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | any => {
+      return this.address ? null : {
+        validateAddress: {
+          valid: false
+        }
+      };
+    }
+  }
+
+  public subscribe(eventEmitter: EventEmitter<any>) {
+    this.subscription = eventEmitter.subscribe((address: PlaceResult) => {
+      this.address = address;
+    });
+  }
+
+  public unsubscribe() {
+    this.subscription.unsubscribe();
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  set address(value) {
+    this._address = value;
+  }
+}
